73432143bc024f97d0be42f38c9899b921d50e4a
Saugnik <148228363+saugnik@users.noreply.github.com>
2025-04-24 01:43:36 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,6 +23,7 @@ const Navbar = () => {
           className="lg:hidden text-white"
           onClick={toggleMobileMenu}
           aria-label="Toggle menu"
+          
         >
           <Menu size={28} />
         </button>
